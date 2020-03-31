dfdcb6efd3ca29b89644dd6dff14f1895d895f02
Merflesk <joachim.mathisen@gmail.com>
2020-03-31 22:11:54 +0200

    added hanoi-function

--- hanoi.c
+++ hanoi.c
@@ -1,11 +1,12 @@
 /*
 Author: Joachim Larsen Mathisen
 Program: Tower of Hanoi
-
+ 
   =         |        |
   ==        |        |
   ===       |        |
 #######  #######  #######
+   A        B        C
 
 Rules:
 The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
@@ -17,11 +18,34 @@ The objective of the puzzle is to move the entire stack to another rod, obeying 
    solve a Tower of Hanoi puzzle is 2n − 1, where n is the number of disks.
 */
 
+/*
+
+*/
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+void hanoi(int n, char from_rod, char to_rod, char aux_rod)
 {
-    /* code */
+    // n = num of disk, from_rod = where the disk is now, to_rod = where the disk goes to, aux_rod = the extra rod
     
+    // We only move one disk at a time
+    if (n == 1)
+    {
+        printf("Move disk 1 from rod %c to rod %c", from_rod, to_rod);
+    }
+
+    // function calls itself
+    // First, we move top disk from A to C
+    hanoi(n-1, from_rod, aux_rod, to_rod);
+    printf("Move disk %d from %c to %c", n, from_rod, aux_rod);
+    // Second, we move the top disk of C to B
+    hanoi(n-1, aux_rod, to_rod, from_rod);
+
+}
+
+int main()
+{
+    /* Starting point of the program */
+    int n = 4 // Sets the number of disks in play
+    hanoi(n, 'A', 'C', 'B'); // A, B, C are the rods. We start by moving top of A to C
     return 0;
 }
